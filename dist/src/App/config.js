c73c73a76d86984ae563de4cce64e13c181fa1d6
André Fernandes <andrefcasimiro@awayco.com>
2019-08-28 14:52:24 +0100

    Deleted dist/ files

--- dist/src/App/config.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = {
-    corsOptions: {
-        origin: `http://localhost:3000`,
-        optionsSuccessStatus: 200,
-    },
-};
